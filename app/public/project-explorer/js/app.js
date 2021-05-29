3cbda4d6f3130d5231eaaf9ab0ea0f510f10b312
Bost <opeyemit@activedgetechnologies.com>
2021-05-29 13:28:20 +0100

    modified viewproject1

--- app/public/project-explorer/js/app.js
+++ app/public/project-explorer/js/app.js
@@ -297,11 +297,13 @@ const fetchGraduationYears = async () => {
   //process viewproject 
   
       const viewProjectById = async () =>{
-        const user = await createdBy();
+        // const user = await createdBy();
         let paramString = window.location.href;
         var searchParam = new URLSearchParams(paramString);
         var projectId = searchParam.get("http://localhost:4000/project-explorer/viewproject.html?id");
-        let url= `/api/projects/${projectId}`;
+        let params = new URLSearchParams(document.location.search.substring(1));
+        let pId = params.get("id");
+        let url= `/api/projects/${pId}`;
         await fetch(url,{
             headers: {
                 'Content-Type': 'application/json'
