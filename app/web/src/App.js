3f34ccfa8127a21b63994e84e10d221ae93ad4bc
Bost <opeyemit@activedgetechnologies.com>
2021-06-27 21:29:24 +0100

    Completed Week 7  Excercise

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -17,8 +17,8 @@ function App() {
         <Route path="/" exact={true} component={Home}/>
         <Route path="/signup" component={Signup}/>
         <Route path="/login" component={Login}/>
-        <Route exact={true} path="/projects/:id" component={Project}/> 
         <Route exact={true} path="/projects/submit" component={CreateProject}/> 
+        <Route exact={true} path="/projects/:id" component={Project}/> 
         <Route path="/editproject" component={EditProject}/>
         <Route path="*" render={()=><Layout><div className="text-center p-5"><h1>Oops!!! Page not found</h1></div></Layout> }/>
       </Switch>
